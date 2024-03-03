656a426d6e1831417f3fb64ef1c27a3b9a454b64
Ahmednur <ahmednur.khaliif0004@stud.hkr.se>
2024-03-03 14:40:13 +0100

    new commit

--- History.py
+++ History.py
@@ -1,22 +1,32 @@
 import player
 
 class History_players:
+    self.__player_data = {}
+    self.__filename = "player_stats.bin"
     
-    Player_data = {}
-
+    
     def addPlayer(self, player):
-        if player.name in self.Player_data:
-            self.Player_data[player.name].append((player.get_winns(), player.get_losses()))
+        if player.name in self.__player_data:
+            self.__player_data[player.name].append((player.__wins, player.__loss))
         else:
-            self.Player_data[player.name] = [(player.get_winns(), player.get_losses())]
+            self.__player_data[player.name] = [(player.__wins, player.__loss)]
 
-    def print_stats(self):
+    def print_stats(self , player_object):
+        if player_object in self.__player_data:
+            for player_name, game_stats in self.__player_data.items():
+                print("Player Name:", player_name)
+                for wins, losses in game_stats:
+                   print("Wins: ", wins)
+                   print("Losses: ", losses)
+
     
-        for player_name, game_stats in self.Player_data.items():
-            print("Player Name:", player_name)
-            for wins, losses in game_stats:
-                print("Wins: ", wins)
-                print("Losses: ", losses)
+    def save_players_data(self):
+        with open(self.__filename, 'wb') as file:  # Use 'wb' for writing in binary mode
+            pickle.dump(self.__player_data, file)
+
+    def load_players_list(self):
+        with open(self.__filename, 'rb') as file:  # Use 'rb' for reading in binary mode
+            return self.__player_data == pickle.load(file)
 
 
 
--- compter.py
+++ compter.py
@@ -7,41 +7,154 @@ class Computer:
         self.gamewon = 0 
         self.gameloss = 0 
         
-        
-    def update_score(self,score):
-        self.points += score
-        
-    def rolldice ():
-        die = dice.Dice()
-        return die.roll
     
-    def hold(self, score):
-        self.update_score(score)
-        self.turn_score = 0
+    def rollDice (self):
+        die = dice.Dice
+        rolled_dice = die.roll
+        return rolled_dice    
+    
+    def update_score(self, roll_dice_num):
         
-    def winner (self):
-        self.gamewon += 1
-        
+        if roll_dice_num == 1: 
+            self.__totalscore = 0 
+            
+        else:
+            self.__totalscore += roll_dice_num
+            
+    def hold ():
+        pass
+    
+    def won (self):
+        self.__wins += 1
+        self.__gameplayed += 1
+    
     def lost(self):
-        self.gameloss += 1
-     
-    def get_name(self):
-        return self.name
-    
-    def get_wins(self):
-        return self.gamewon
-    
-    def get_losses(self):
-        return self.gameloss
-    
-    # The stragies for easy , normal and hard 
-    
-    def easyMode ():
+        self.__loss += 1
+        self.__gameplayed += 1
+        
+        
+    def easyMode(self):
         pass
     
-    def normalMode ():
+    def mediumMode (self):
         pass
     
-    def hardMode ():
+    def hardMode (self):
         pass
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    #     self.__comp_score = 0
+    #     self.__option = ''
+    #     if isinstance(comp, Player):
+    #         self.__comp = comp
+    #     else:
+    #         raise ValueError("The 'comp' parameter must be an instance of Player.")
+
     
+    # def score (self, roll_dice_num):
+    #     total=0
+    #     if roll_dice_num !=1: 
+    #         total += roll_dice_num
+    #         self.__score += total
+    #         return self.__comp_score
+    #     else :
+    #         return self.__comp_score
+        
+    
+    # def Beginner(self, score):
+    #     strategy = random.randint(1,3)   # Strategy 1 = "Hold at 20", Strategy 2 = "Hold at 25", Strategy 3 = "Fair Play"
+    #     if strategy == 1:
+    #         if score < 20:
+    #             return self.__option == 'P'
+    #         else:
+    #             return self.__option == 'H'
+    #     elif strategy == 2:
+    #         if score < 25:
+    #             return self.__option == 'P'
+    #         else:
+    #             return self.__option == 'H'
+    #     else :
+    #         hold_play = random.choice (['H', 'P'])
+    #         return hold_play
+            
+            
+    def Advance (self, score, opponent_score):
+        
+        if self.__comp_score == 0 and opponent_score == 0 :
+            self.__option == 'P'
+            if score < 20:
+                return self.__option == 'P'
+            else:
+                return self.__option == 'H'
+        elif self.__comp_score >= 25 :
+            target_score = 100 - opponent_score
+         
+            return self.__option == 'H'
+            return self.turn_total < target_score / num_turns
+        #End race or keep race
+        elif self.__comp_score > 71 or p1_score > 71 :
+            return self.__option == 'P'
+        else:
+            diff_bet_scores = 21 + (self.__comp_score - p1_score)/8
+            
+            
+        
+        option = random.randint(1,2)
+        hold_play = random.choice (['H', 'P'])
+        if option == 1:
+            if score < 25:
+                return self.__option == 'P'
+            else:
+                return self.__option == 'H'
+        else:
+            if score < 25:
+                return self.__option == 'P'
+            else:
+                return self.__option == 'H'
